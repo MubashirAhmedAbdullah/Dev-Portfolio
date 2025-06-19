1021b2c1919a7f63e2dd47ff8a06be5198e9b0fe
Mubashir Ahmed Abdullah <mubashirahmed5468@gmail.com>
2025-06-19 14:02:57 +0500

    statring new porfolio

--- data/index.ts
+++
@@ -1,8 +0,0 @@
-
-
-export const GridItemsData = [
-    { title: "Title 1", description: "Desc 01", id: 1 },
-    { title: "Title 2", description: "Desc 02", id: 2 },
-    { title: "Title 3", description: "Desc 03", id: 3 },
-    { title: "Title 4", description: "Desc 04", id: 4 },
-]
